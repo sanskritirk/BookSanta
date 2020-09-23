c61bfe8cca1057cbcd8ea83cd38302e8d6a9654b
sanskritirk <kanagalasanskriti@gmail.com>
2020-09-23 22:25:43 +0530

    Project

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen';
 
-export default function App() {
+export default class App extends React.Component {
+  render(){  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>   
+    <WelcomeScreen/> 
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
---
+++ config.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+require ('@firebase/firestore')
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDgaxpm3SygfHxva1r7Sad-ZicU9raSE2w",
+    authDomain: "booksanta-c22ae.firebaseapp.com",
+    databaseURL: "https://booksanta-c22ae.firebaseio.com",
+    projectId: "booksanta-c22ae",
+    storageBucket: "booksanta-c22ae.appspot.com",
+    messagingSenderId: "682407911506",
+    appId: "1:682407911506:web:efd81cb9616f7ee1d4ff8b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
